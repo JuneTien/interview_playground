93c322b9acfda7f898a7ffe9fbbd8e606a3e7e43
June Tien <chiachun.tien@gmail.com>
2021-04-23 15:09:53 +0800

    陣列隨機重新排列

--- src/index.js
+++ src/index.js
@@ -50,3 +50,39 @@ function fib(indexNum) {
 }
 
 console.log("Fib: ", fib(5));
+
+// 隨機重新排列
+// Solutio 1: 此寫法會有效能問題：若隨機 index 一直都取到相同的數值時
+function reRandom(input) {
+  let ret = [];
+  let inputArr = input;
+
+  for (let i = 0; i < inputArr.length; i++) {
+    console.log("Solution 1");
+    let index = Math.floor(Math.random() * inputArr.length);
+    if (inputArr[index] !== -1) {
+      ret.push(inputArr[index]);
+      inputArr[index] = -1;
+    } else {
+      i--;
+    }
+  }
+  return ret;
+}
+
+// Solution 2
+function reRandom2(input) {
+  let ret = [];
+  let inputArr = input;
+  let inputArrLegth = inputArr.length;
+  for (let i = 0; i < inputArrLegth; i++) {
+    console.log("Solution 2");
+    const randomNum = inputArr[Math.floor(Math.random() * inputArr.length)];
+    ret.push(randomNum);
+    inputArr.splice(inputArr.indexOf(randomNum), 1);
+  }
+  return ret;
+}
+
+console.log("reRandom: ", reRandom([1, 3, 4, 5, 7, 0, 9]));
+console.log("reRandom: ", reRandom2([1, 3, 4, 5, 7, 0, 9]));
